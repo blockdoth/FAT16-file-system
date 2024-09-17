f2a0e6ddf0b9bed58c94d9762c9af11e7d2d5b7c
blockdoth <pepijn.pve@gmail.com>
2024-09-17 14:11:10 +0200

    nixified it a bit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix flake template for a C development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          # stdenv = pkgs.clangStdenv;  # Change standard compiler
+          shellHook = "
+            echo 'Entering C shell template'
+            echo 'Location of gcc => ${pkgs.gcc}'
+            echo 'Location of cmake => ${pkgs.cmake}'
+            echo 'Location of valgrind => ${pkgs.valgrind}'
+          ";
+          packages = with pkgs; [
+
+            cmake 
+            gcc
+            valgrind
+          ] ++ (if system == "aarch64-darwin" then [ ] else [ 
+            gdb 
+          ]);
+        };
+      });
+    };
+}
